bdd8f5937804e17b1abdcc67abc9ec5cfadd0233
simplon-marseille <acardnicolas@hotmail.com>
2020-11-11 22:49:15 +0100

    mise à jour

--- flight.js
+++ flight.js
@@ -32,7 +32,7 @@ const vols = [{
 },
 {
   destination: 'Tokyo',
-  date_depart: '05/12/2019',
+  date_depart: '12/12/2020',
   compagnie_aerienne: 'Air france',
   numVol: 'xxxxx',
   prix: '750',
@@ -40,7 +40,7 @@ const vols = [{
 },
 {
   destination: 'Tokyo',
-  date_depart: '02/12/2020',
+  date_depart: '12/11/2020',
   compagnie_aerienne: 'Lufthansa',
   numVol: 'xxxxx',
   prix: '850',
@@ -59,106 +59,38 @@ const vols = [{
 //Afficher tous les vols dont le tarif est inférieur à 700 euros et qui
 //voyagent avec Air france
 
-//Afficher tous les vols par date :
-//à partir de la date d'aujourd'hui
-
-//Afficher tous les vols par date :
-//avant la date d'aujourd'hui
+//Afficher tous les vols qui voyagent avec 'Lufthansa' dont le prix est inférieur à 900 euros
+//et dont la durée du trajet est inférieur à 15 heures
 
 //Appliquer une reduc de 20% sur chaque vol organiser après 2019 et les afficher
 
-//Fonction permettant de comparer deux date et renvoie 'true' si date1 est supérieur à date2
+//Afficher tous les vols pour Paris prévus entre le "06/02/2019" et le "09/06/2019".
+
+//----------------------------------------------------------------------------------
+
+
+
+//Afficher tous les vols à partir de la date d'aujourd'hui
+
+//Un petit coup de main...
+//Il faut récupèrer la date du jour avec l'objet 'date' (new Date()) fournis par Javascript.
+
+//La fonction suivante permet de comparer deux dates et renvoie 'true' si date1 est supérieur à date2
+//Pour comparer deux, celles-ci doivent avoir le même format.
 const date1SupDate2 = (date1, date2) => {
-  // L'objet 'Date' permet de pouvoir comparer deux dates.
-  date1 = new Date(date1);
-  date2 = new Date(date2);
-  if(date1 > date2){
+  startDate = new Date(date1);
+  endDate = new Date(date2);
+  if (startDate > endDate) {
     return true;
-  }else{
+  } else {
     return false;
   };
 };
 
-vols.forEach((vol) => {
-  if(date1SupDate2(vol.date_depart, '01/01/2020')){
-    //si la comaraison renvoie 'true' on affiche les destinations
-    console.log(`Le vol à destination de ${vol.destination} est désormais à ${vol.prix * 0.8} euro au lieu de ${vol.prix} euros`);
-  };
-});
 
-//Afficher tous les vols pour Paris prévus entre le "06/02/2019" et le "09/06/2019".
-
-//Afficher tous les vols qui voyagent avec 'Lufthansa' dont le prix est inférieur à 900 euros
-//et dont la durée du trajet est inférieur à 15 heures
-
-
+console.log(date1SupDate2("12/13/2020", "12/12/2020"));
 
 
 
 //Allez voir ce qu'est un JSON !
 //https://developer.mozilla.org/fr/docs/Learn/JavaScript/Objects/JSON
-
-
-
-
-// const tasse1 = {
-//   poignet: 'oui',
-//   couleur: 'bleu',
-//   taille: '10cm',
-//   marque: 'maison du monde'
-// }
-
-// const tasse2 = {
-//   poignet: 'oui',
-//   couleur: 'jaune',
-//   taille: '10cm',
-//   marque: 'conforama'
-// }
-
-// const tasse3 = {
-//   poignet: 'oui',
-//   couleur: 'jaune',
-//   taille: '10cm',
-//   marque: 'maison du monde'
-// }
-
-//const tasses = [tasse1, tasse2, tasse3];
-
-//format de données JSON
-
-
-const tasses = [
-{
-  poignet: 'oui',
-  couleur: 'bleu',
-  taille: '10cm',
-  marque: 'maison du monde'
-},
-{
-  poignet: 'oui',
-  couleur: 'jaune',
-  taille: '10cm',
-  marque: 'conforama'
-},
-{
-  poignet: 'oui',
-  couleur: 'jaune',
-  taille: '10cm',
-  marque: 'maison du monde'
-}
-]
-
-
-// tasses.forEach((tasse) => {
-//   if(tasse.marque === 'maison du monde'){
-//     console.log(tasse)
-//   }
-// });
-
-
-
-
-
-
-
-
